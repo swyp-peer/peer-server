e94377235bf3e11e82693f5c31caca00985c5198
Haeun-Seo <cindy7153@gmail.com>
2024-05-02 02:42:51 +0900

    Update user data.

---
+++ src/main/java/com/example/peer/user/dto/UpdateUserRequest.java
@@ -0,0 +1,23 @@
+package com.example.peer.user.dto;
+
+public class UpdateUserRequest {
+    private String name;
+    private String profileImageUrl;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+}
