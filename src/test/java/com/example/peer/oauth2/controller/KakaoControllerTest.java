b1fb5b0a790f915c7813fc0123a56df37324773c
tngtied <tngtied@gmail.com>
2024-05-09 01:13:41 +0900

    [FIX] 통일되지 않은 User Entity Builder의 ProfileImageUrl 필드로 인한 빌드 실패 수정

--- src/test/java/com/example/peer/oauth2/controller/KakaoControllerTest.java
+++ src/test/java/com/example/peer/oauth2/controller/KakaoControllerTest.java
@@ -4,8 +4,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
@@ -13,9 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.peer.security.entity.TokenInfo;
@@ -26,7 +21,6 @@ import com.example.peer.user.entity.Role;
 import com.example.peer.user.entity.User;
 import com.example.peer.user.repository.UserRepository;
 
-import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 
 @SpringBootTest
@@ -43,12 +37,11 @@ class KakaoControllerTest {
 	private UserRepository userRepository;
 	private static final String base_mapping = "/oauth2/kakao";
 
-
 	String getAccessToken(String username) {
 		User user = User.builder()
 			.name(username)
 			.socialId("1")
-			.profileImage("/")
+			.profileImageUrl("/")
 			.role(Role.MENTEE)
 			.oauthType(OauthType.KAKAO)
 			.build();
@@ -69,10 +62,10 @@ class KakaoControllerTest {
 
 	@Test
 	@DisplayName("Access Token Test")
-	void accessTokenTest() throws Exception{
+	void accessTokenTest() throws Exception {
 		String accessToken = getAccessToken("username");
 		mockMvc.perform(get("/nothing")
-			.header("Authorization", "Bearer " + accessToken))
+				.header("Authorization", "Bearer " + accessToken))
 			.andExpect(status().isNotFound())
 			.andDo(print());
 	}
